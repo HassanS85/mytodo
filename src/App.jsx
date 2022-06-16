706850435cdd8f5cd874790140cd78d1ed82ebe7
Hassan <hassan.seedat07@gmail.com>
2022-06-16 11:53:38 +0100

    added in handleclick function and returned new input to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 
 const App = () => {
   const [listItem, setListItem] = useState("");
   const [todo, setTodo] = useState([
-    { isIncomplete: "5km bike ride", isComplete: true },
-    { isIncomplete: "Fix door handle", isComplete: false },
-    { isIncomplete: "Complete maths assignment", isComplete: true },
-    { isIncomplete: "Buy fruit", isComplete: false },
-    { isIncomplete: "Pick up kids from swimming lesson", isComplete: false },
+    { isIncomplete: "5km bike ride", isCompleted: true },
+    { isIncomplete: "Fix door handle", isCompleted: false },
+    { isIncomplete: "Complete maths assignment", isCompleted: true },
+    { isIncomplete: "Buy fruit", isCompleted: false },
+    { isIncomplete: "Pick up kids from swimming lesson", isCompleted: false },
   ]);
 
 const newListItem = (listItem) => {
@@ -32,17 +32,27 @@ const newListItem = (listItem) => {
     setTodo(arr);
   }
 
-return (
-  <div></div>
-)
+  const handleClick = (event) => {
+    setListItem(event.target.value);
+  };
 
 
+  return (
+    <div>
+      <input
+        className="todo-input"
+        value={listItem}
+        onClick={(handleClick)}
+        onKeyPress={e => {
+          if (e.key === "Enter") {
+            newListItem(listItem);
+            setListItem("");
+          }
+        }}
+      />
+      
+}
 
 
-
-
-
-
-};
 
 export default App;
