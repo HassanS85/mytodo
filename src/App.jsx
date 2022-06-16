23ec4920cc16de2e722051b080a7c90037d95217
Hassan <hassan.seedat07@gmail.com>
2022-06-16 11:09:49 +0100

    created truthy falsy values in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  
-    
-  ;
-}
+const App = () => {
+  const [listItem, setListItem] = useState("");
+  const [todo, setTodo] = useState([
+    { incomplete: "5km bike ride", isComplete: true },
+    { incomplete: "Fix door handle", isComplete: false },
+    { incomplete: "Complete maths assignment", isComplete: true },
+    { incomplete: "Buy fruit", isComplete: false },
+    { incomplete: "Pick up kids from swimming lesson", isComplete: false },
+  ]);
+
+const newListItem = (listItem) => {
+    let arr = [...todo];
+    arr.push({
+      incomplete: listItem,
+      isCompleted: false
+    });
+    setTodo(todosArray);
+  }
+
+};
 
 export default App;
